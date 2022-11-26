5915bbce53bc42b2bc8597c2fd624bc59a5ac6c4
RohanNafde <rohannafde212@gmail.com>
2022-11-27 00:04:43 +0530

    Update FSM.vhd

--- FSM.vhd
+++ FSM.vhd
@@ -2,14 +2,17 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity fsm is
-	port(clock, reset: in std_logic; op_code: in std_logic_vector (3 downto 0);  
+	port(clock, reset, carry, zero: in std_logic;
+			op_code: in std_logic_vector (3 downto 0);
+			imm: in std_logic_vector (7 downto 0);
+			lsb: in std_logic_vector(1 downto 0);
 			C2, C1, C0: out std_logic  );
 end fsm;
 
 architecture bhv of fsm is
 
 type statetype is (S1, S2, S3, S4, S5, S6, S7, S8);
-signal ps, ns : statetype;  
+signal ps, ns: statetype;  
 
 begin 
 
@@ -23,52 +26,60 @@ begin
 	end process update;
 
 	next_state_output_logic: process (ps, op_code)
+	variable count: integer;
 	begin 
 		case ps is 
 
 		when S1 =>
-			if op_code = "0011" or op_code = "1001" or op_code = "1000" then 
+			count := 0;
+			if (op_code = "0011" or op_code = "1001" or op_code = "1000") then 
 				ns <= S4;
 			else 
 				ns <= S2;
 			end if;
 
 		when S2 =>
-			if op_code = "0001" or op_code = "0000" then 
+			if ((op_code = "0001" or op_code = "0000") and 
+				((lsb(1) = '1' and carry = '0') or (lsb(0) = '0' and zero = '0'))) then 
 				ns <= S1;
 			else 
 				ns <= S3;
 			end if;
 
 		when S3 =>
-			if op_code(3) = '1' or op_code(2) = '1' then 
+			if ((op_code(3) = '1' and op_code(2) = '1' and zero = '1') or count > 7) then
+				ns <= S1;
+			elsif (op_code(3) = '1' or op_code(2) = '1') then 
 				ns <= S6;
-			elsif op_code(2) = '1' or op_code(0) = '1' then 
+			elsif (imm(count) = '0') then
+				ns <= S3;
+			elsif (op_code(2) = '1' or op_code(0) = '1') then 
 				ns <= S5;
 			else 
 				ns <= S4;
 			end if;
+			count := count + 1;
 
 		when S4 =>
-			if op_code(2) = '1' or op_code(1) = '1' then 
+			if (op_code(2) = '1' or op_code(1) = '1') then 
 				ns <= S3;
-			elsif op_code = "1000" or op_code = "1010" then 
+			elsif (op_code = "1000" or op_code = "1010") then 
 				ns <= S6;
-			elsif op_code = "1001" or op_code = "1011" then 
+			elsif (op_code = "1001" or op_code = "1011") then 
 				ns <= S8;
 			else 
 				ns <= S1;
 			end if;
 
 		when S5 =>
-			if op_code(2) = '1' or op_code(1) = '1' then 
+			if (op_code(2) = '1' or op_code(1) = '1') then 
 				ns <= S3;
 			else 
 				ns <= S1;
 			end if;
 
 		when S6 =>
-			if op_code(3) = '1' or op_code(2) = '0' then 
+			if (op_code(3) = '1' or op_code(2) = '0') then 
 				ns <= S1;
 			else 
 				ns <= S7;
